5e5395efbadf590301c59a07110a7cb5cf143c93
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-13 15:44:47 -0300

    csharpier no blazor JA!

--- Imc/Program.cs
+++ Imc/Program.cs
@@ -1,11 +1,14 @@
+using Imc;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Imc;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped(sp => new HttpClient
+{
+    BaseAddress = new Uri(builder.HostEnvironment.BaseAddress)
+});
 
 await builder.Build().RunAsync();
