e25f5d9fec182c57620269cc074358aa4e5bb8a0
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-13 17:43:38 -0300

    validação de cadastro

--- Imc/wwwroot/js/formsCheck.js
+++ Imc/wwwroot/js/formsCheck.js
@@ -26,13 +26,40 @@ window.checkingRecuperarLoginForms = function () {
 window.checkingCadastroForms = function () {
     let email = document.getElementById("email").value;
     let notRobot = document.getElementById("notRobot").checked;
+    let senha = document.getElementById("senha").value;
+    let confirmaSenha = document.getElementById("confirmaSenha").value;
 
     if (email.trim() === "" || !notRobot) {
         alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+    else if (senha !== confirmaSenha) {
+        alert("Confirmação da senha não é igual.");
+        return false;
+    }
+    else if (senha.lenght < 8) {
+        alert("Senha tem que haver no minimo 16 caracteres.");
+        return false;
     }
     else {
-        window.location.href = '/calculadora'
+        return true;
     }
+}
+
+window.cadastroDados = function () {
+    let data = "Email=" + document.getElementById("email").value + "/";
+    data += "Senha=" + document.getElementById("senha").value + "/";
+
+    return data;
+}
+
+window.finalizarCadastro = function () {
+    alert("Conta cadastrada com Sucesso!");
+    window.location.href = '/calculadora';
+}
+
+window.falhaCadastro = function () {
+    alert("Este Email já tem um cadastro no app.");
 }
 
 window.chekingCalculoValores = function (altura, peso, sexo) {
