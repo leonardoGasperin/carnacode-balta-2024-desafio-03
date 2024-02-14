fb89937f336842676508c4da204feccc5ccef907
leonardoGasperin <leonardovdg@hotmail.com>
2024-02-13 23:24:19 -0300

    release

---
+++ Imc/dist/wwwroot/js/formsCheck.js
@@ -0,0 +1,89 @@
+window.checkingLoginForms = function () {
+    let email = document.getElementById("email").value;
+    let senha = document.getElementById("senha").value;
+    let notRobot = document.getElementById("notRobot").checked;
+
+    if (email.trim() === "" || senha.trim() === "" || !notRobot) {
+        alert("Por favor, preencha todos os campos.");
+    }
+}
+
+window.checkingRecuperarLoginForms = function () {
+    let email = document.getElementById("email").value;
+    let notRobot = document.getElementById("notRobot").checked;
+
+    if (email.trim() === "" || !notRobot) {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+
+    return true;
+}
+
+window.checkingCadastroForms = function () {
+    let email = document.getElementById("email").value;
+    let notRobot = document.getElementById("notRobot").checked;
+    let senha = document.getElementById("senha").value;
+    let confirmaSenha = document.getElementById("confirmaSenha").value;
+
+    if (email.trim() === "" || !notRobot) {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+    else if (senha !== confirmaSenha) {
+        alert("Confirmação da senha não é igual.");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
+
+window.cadastroDados = function () {
+    let data = "Email:" + document.getElementById("email").value + "|";
+    data += "Senha:" + document.getElementById("senha").value;
+
+    return data;
+}
+
+window.finalizarCadastro = function () {
+    alert("Conta cadastrada com Sucesso!");
+    window.location.href = '/calculadora';
+}
+
+window.falhaCadastro = function () {
+    alert("Este Email já tem um cadastro no app.");
+}
+
+window.recuperarConta = function (data) {
+    if (!data || data.length === 0) {
+        alert("Nenhuma conta encontrada com este E-mail.");
+        return;
+    }
+
+    const regex = /Senha:\s*([^|]*)/;
+    const match = data.match(regex);
+
+    if (match && match[1]) {
+        const passwordValue = match[1].trim();
+        alert("Valor da senha: " + passwordValue);
+    }
+}
+
+window.login = function (usrData) {
+    if (!usrData || usrData.length === 0) {
+        alert("Dados não batem com nenhum registro. Porfavor, checar seus dados ou se preferir Crie uma conta =)");
+        return;
+    }
+    window.location.href = '/calculadora';
+}
+
+window.chekingCalculoValores = function (altura, peso, sexo) {
+    if (altura <= 0 || peso <= 0) {
+        alert("Por favor, preencha todos os campos.");
+        return false;
+    }
+    else {
+        return true;
+    }
+}
